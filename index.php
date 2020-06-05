c5c61b54f560053577d20fe349301d14774f9c89
ThomasMurat <thomasmurat1992@gmail.com>
2020-06-05 14:56:53 +0200

    up exo4

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Php-Partie1-exo4</title>
+    </head>
+    <body>
+        <?php 
+            $varint = 7;
+            $varstring = 'texte';
+            $varfloat = 12.5;
+            $varbool = true;
+        ?>
+        <p><?php echo $varstring; ?></p>
+        <p><?php echo $varint; ?></p>
+        <p><?php echo $varfloat; ?></p>
+        <p><?php echo $varbool; ?></p>
+    </body>
+</html>
